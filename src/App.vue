52f10fdac785693aa5555480560f36c01dab11b7
mejasonatkinson <me@jasonatkinson.co.uk>
2026-05-14 19:53:58 +0100

    Remove SingleSitePageV2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 // import SuperSimplePage from './components/5-pages/SuperSimplePage.vue';
 import SingleSitePage from './components/5-pages/SingleSitePage.vue';
-// import SingleSitePageV2 from './components/5-pages/SingleSitePageV2.vue';
 </script>
 
 <template>
   <!-- <SuperSimplePage /> -->
    <SingleSitePage />
-   <!-- <SingleSitePageV2 /> -->
 </template>
--- src/components/1-atom/TagBadge.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-defineProps({
-    label: String,
-});
-</script>
-
-<template>
-    <span class="tag inline-block mb-2 text-xs text-color-off-white bg-color-off-black px-2 py-1 rounded mr-2">{{ label }}</span>
-</template>
